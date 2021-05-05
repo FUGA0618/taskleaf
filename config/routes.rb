7053e4ed31267b2fc3ab22104ae13e2574eabf66
FUGA0618 <aax.chiri@gmail.com>
2021-05-05 10:02:07 +0900

    bin/rails g controller Admin::Users new edit show index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'users/new'
+    get 'users/edit'
+    get 'users/show'
+    get 'users/index'
+  end
   root to: 'tasks#index'
   resources :tasks
 end
