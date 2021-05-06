c9f5dea07efc515eb689f94789ac76e3df85928c
FUGA0618 <aax.chiri@gmail.com>
2021-05-06 19:25:00 +0900

    ログイン機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'login', to: 'sessions#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
 
   namespace :admin do
     resources :users
