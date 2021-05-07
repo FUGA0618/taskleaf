9abdf391f52f42b2a1462fc48109fedebc61c4b0
FUGA0618 <aax.chiri@gmail.com>
2021-05-07 20:54:35 +0900

    アクション実行前にタスクインスタンスを生成する処理をメソッド化

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,12 @@
 # frozen_string_literal: true
 
 class TasksController < ApplicationController
+  before_action :set_task, only: %i[show edit update destroy]
   def index
     @tasks = current_user.tasks.order(created_at: :desc)
   end
 
-  def show
-    @task = current_user.tasks.find(params[:id])
-  end
+  def show; end
 
   def new
     @task = Task.new
@@ -23,19 +22,15 @@ class TasksController < ApplicationController
     end
   end
 
-  def edit
-    @task = current_user.tasks.find(params[:id])
-  end
+  def edit; end
 
   def update
-    task = current_user.tasks.find(params[:id])
-    task.update(task_params)
+    @task.update(task_params)
     redirect_to tasks_url, notice: "タスク「#{task.name}」を更新しました。"
   end
 
   def destroy
-    task = current_user.tasks.find(params[:id])
-    task.destroy
+    @task.destroy
     redirect_to tasks_url, notice: "タスク「#{task.name}」を削除しました。"
   end
 
@@ -43,5 +38,9 @@ class TasksController < ApplicationController
 
   def task_params
     params.require(:task).permit(:name, :description)
+  end
+
+  def set_task
+    @task = current_user.tasks.find(params[:id])
   end
 end
